5abfcd2f7fe51a366d4fc614f5773a9493f73212
Aavash232311 <aavash3150@gmail.com>
2026-02-20 14:02:52 -0600

    Change of logic in system design

--- CollegeApp.Server/Models/Confession.cs
+++ CollegeApp.Server/Models/Confession.cs
@@ -20,6 +20,16 @@ namespace CollegeApp.Server.Models
         public ICollection<Comments> Comments { get; set; } = new List<Comments>(); // one confession can have many comments, so we can use list here
         /* Rest of the related model like, likes and comments we can keep in different database model */
         public bool deleted { get; set; } = false;
+        public Guid? referenceId { get; set; }
+        /* This for extending the confession so that the user can continue in next page.
+         We will create a new confession referencing the previous confession,
+        Okay duplicate is better than the wrong abstraction, but 
+        (still sad I couldn't land an intern) 
+        what if we have lot's of user driven data, so just reference it
+        create an interface and make it work!.
+        Set hard limit at the end
+        */
+        public Confession? ExtendedThreadOfConfession { get; set; }
     }
 }
 // https://learn.microsoft.com/en-us/ef/core/modeling/relationships/one-to-many
